e492db4c4fc6b0bea056f1a89d279a16df2fd713
Shyaam <shanmugams@ufl.edu>
2026-01-23 10:43:54 -0500

    Initial Commit

---
+++ Pickle_Rick/Pickle_RickApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pickle_RickApp.swift
+//  Pickle_Rick
+//
+//  Created by Shyaam Shanmugam on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Pickle_RickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
